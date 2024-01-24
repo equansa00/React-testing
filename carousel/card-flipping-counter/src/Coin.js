9e43c31113842de23d70437d868ed42c21bab659
equansa00 <nanaquansah@gmail.com>
2024-01-24 07:27:51 -0500

    created card_flipping_counter

--- carousel/card-flipping-counter/src/Coin.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function Coin({ side }) {
-  const imageUrl = side === "heads" ? "p/home/equansa00/Desktop/UMass/react-state-carousel-coins (copy)/carousel/card-flipping-counter/CoinHead.jpg" : "/home/equansa00/Desktop/UMass/react-state-carousel-coins (copy)/carousel/card-flipping-counter/CoinTail.jpg";
-  return <img src={imageUrl} alt={side} />;
-}
-
-export default Coin;
